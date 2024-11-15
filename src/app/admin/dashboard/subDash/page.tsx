221a3c5febe49c1567791033b3513215855700fc
Mohana Prakash <mohan@ggexcel.ai>
2024-11-15 18:49:34 +0530

    added suspense3

--- src/app/admin/dashboard/subDash/page.tsx
+++ src/app/admin/dashboard/subDash/page.tsx
@@ -1,27 +1,12 @@
 "use client";
 
 import React, { Suspense } from "react";
-import { useSearchParams, usePathname, useRouter } from "next/navigation";
+import SubDashContent from "./subDashContent";
 
 export default function SubDashPage() {
-  const router = useRouter();
-  const searchParams = useSearchParams();
-  const pathname = usePathname();
-  const id = searchParams.get("id");
-
-  console.log(id, pathname);
-
   return (
     <Suspense fallback={<div>Loading SubDash...</div>}>
-      <div>
-        <h1>Sub Dashboard</h1>
-
-        {id ? (
-          <p onClick={() => router.back()}>Sub Dashboard ID: {id}</p>
-        ) : (
-          <p>No ID specified</p>
-        )}
-      </div>
+      <SubDashContent />
     </Suspense>
   );
 }
---
+++ src/app/admin/dashboard/subDash/subDashContent.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import React from "react";
+import { useSearchParams, usePathname, useRouter } from "next/navigation";
+
+export default function SubDashContent() {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const id = searchParams.get("id");
+
+  console.log(id, pathname);
+
+  return (
+    <div>
+      <h1>Sub Dashboard</h1>
+      {id ? (
+        <p onClick={() => router.back()}>Sub Dashboard ID: {id}</p>
+      ) : (
+        <p>No ID specified</p>
+      )}
+    </div>
+  );
+}
